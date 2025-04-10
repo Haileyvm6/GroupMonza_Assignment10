e5d5a9cc1992def783dc147d315dc29391f14825
Haileyvm6 <haileyvm6@gmail.com>
2025-04-10 10:59:01 -0400

    Created insights.py file

---
+++ cleanPackage/insights.py
@@ -0,0 +1,12 @@
+# File Name : apidata.py
+# Student Name: Joshua Slocumb, Abel Yemaneab, Hailey Manuel, Richie James
+# email:  slocumjt@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date:   04/10/2025
+# Course #/Section: IS 4010-001
+# Semester/Year:  Spring 2025
+# Brief Description of the assignment:This Assignment accesses an API containing informaiton about cities in the US and prints some information about them
+# Brief Description of what this module does. This module accesses the API using a key and brings back the rough data
+# Citations: Rapid API
+
+
